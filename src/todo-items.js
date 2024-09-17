d4bde98d9303a1ddbd8d64c34be884a1be834c23
Fabius Gasber <fabiusgasber@gmail.com>
2024-09-17 11:11:04 -0400

    feat: add todo list item factory

---
+++ src/todo-items.js
@@ -0,0 +1,34 @@
+import { Priority } from "./priorities";
+
+export function createTodo(title = "", description = "", dueDate = null){
+
+    let completed = false;
+    const getCompleted = () => completed;
+    const setCompleted = (hasCompleted) => completed = hasCompleted;
+
+    let priority = new Priority("medium");
+    const getPriority = () => priority.getLevel();
+    const setPriority = (level) => priority.setLevel(level);
+
+    const getDate = () => dueDate;
+    const setDate = (date) => dueDate = new Date(date);
+
+    const getTitle = () => title;
+    const setTitle = (newTitle) => title = newTitle;
+
+    const getDescription = () => description;
+    const setDescription = (newDescription) => description = newDescription;
+
+    return { 
+        getTitle, 
+        setTitle, 
+        getDescription, 
+        setDescription, 
+        getDate, 
+        setDate, 
+        getCompleted, 
+        setCompleted, 
+        getPriority, 
+        setPriority 
+    };
+}
