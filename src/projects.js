2aa9f781e5f66cd1271475f3dfb391571978c42d
Fabius Gasber <fabiusgasber@gmail.com>
2024-09-17 13:41:05 -0400

    feat: add projects factory to keep track of different to do lists

---
+++ src/projects.js
@@ -0,0 +1,15 @@
+export function createProject(title) {
+
+    const getTitle = () => title;
+    const setTitle = (newTitle) => title = newTitle;
+
+    let todoItems = [];
+    const getTodos = () => todoItems;
+    const addTodo = (todo) => todo.isTodoItem ? todoItems.push(todo) : console.warn("Invalid todo item");
+    const removeTodo = (todo) => {
+        const index = todoItems.indexOf(todo);
+        index !== -1 ? todoItems.splice(index, 1) : console.warn("To do item was not found");
+    }
+    
+    return { getTitle, setTitle, getTodos, addTodo, removeTodo }
+}
