b22d42c71fdd8d22b9a73be847f3541f994e6f3f
Fabius Gasber <fabiusgasber@gmail.com>
2024-09-19 14:19:11 -0400

    feat:  add event listeners for adding to dos and switching tabs

--- src/index.js
+++ src/index.js
@@ -4,3 +4,14 @@ function init() {
     setUpListeners();
     loadProjects(getAllTasks());
 }
+
+function setUpListeners() {
+
+    const button = document.querySelector("#addTodo");
+    button.addEventListener("click", initForm);
+
+    const defaultProjects = Array.from(document.querySelectorAll("li"));
+    defaultProjects.forEach(project => project.addEventListener("click", tabSwitch));  
+
+}
+
