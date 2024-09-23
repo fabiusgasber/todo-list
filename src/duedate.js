78082f1ede0d84a07980db7e076bd1bf96f7d506
Fabius Gasber <fabiusgasber@gmail.com>
2024-09-23 16:31:48 -0400

    feat: create due date class

---
+++ src/duedate.js
@@ -0,0 +1,27 @@
+import { parse, format } from "date-fns";
+
+export class DueDate {
+
+    #date 
+
+    constructor(date){
+        this.#date = this.parseDate(date);
+    }
+
+    getDate(){
+        return this.#date;
+    }
+
+    setDate(newDate){
+        this.#date = this.parseDate(newDate);
+    }
+
+    parseDate(date) {
+        return parse(date, "yyyy-MM-dd", new Date());
+    }
+
+    toString() {
+       return isNaN(this.#date.getTime()) ? "" : format(this.#date, "yyyy-MM-dd");
+    }
+
+}
