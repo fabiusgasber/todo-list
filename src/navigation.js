e81e32621339b3ecb2f2d99f9c90cf4764dde539
Fabius Gasber <fabiusgasber@gmail.com>
2024-10-09 15:39:46 +0200

    feat: add class for every default project page

---
+++ src/navigation.js
@@ -0,0 +1,40 @@
+import { domLoader } from "./dom-handler";
+import { project } from "./projects";
+
+export class Navigation {
+    constructor(page){
+        this.page = page;
+    }
+
+    navigateTo(){
+        this.page.navigateTo();
+    }
+}
+
+class Page {
+    navigateTo(){}
+}
+
+export class PageAll extends Page {
+    navigateTo(){
+        domLoader.loadProjects(project.getTodos());
+    }
+}
+
+export class PageToday extends Page {
+    navigateTo(){
+        domLoader.loadProjects(project.getToday());
+    }
+}
+
+export class PageWeek extends Page {
+    navigateTo(){
+        domLoader.loadProjects(project.getWeek());
+    }
+}
+
+export class PageImportant extends Page {
+    navigateTo(){
+        domLoader.loadProjects(project.getImportant());
+    }
+}
