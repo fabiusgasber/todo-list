f5fdabc724f7033d655fb12038e64c8b524db1c1
Fabius Gasber <fabiusgasber@gmail.com>
2024-10-14 21:14:59 +0200

    feat: add get info method to gain access to date and priority classes in user input array

--- src/todo-items.js
+++ src/todo-items.js
@@ -1,15 +1,29 @@
+import { DueDate } from "./duedate";
+import { Priority } from "./priorities";
+
 export function createTodo(inputs){
 
     let completed = false;
     const getCompleted = () => completed;
     const setCompleted = (hasCompleted) => completed = hasCompleted;
 
-    const getInfos = () => inputs;
+    const getInfo = (className) => {
+        if(className && typeof className.prototype === "object"){
+           return inputs.find(input => input instanceof className);
+        }
+        else {
+          return console.warn(`Invalid class name ${className}`);
+        }
+    }
+
+    const getDate = () => getInfo(DueDate);
+    const getPriority = () => getInfo(Priority);
 
     return { 
         getCompleted, 
-        setCompleted, 
-        getInfos,
+        setCompleted,
+        getDate,
+        getPriority,
         isTodoItem : true,
     };
 }
