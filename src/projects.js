62598e12919a255d41d6180071e64353f0f06f85
Fabius Gasber <fabiusgasber@gmail.com>
2024-10-15 12:12:13 +0200

    feat: return todoItem data within projects via object

--- src/projects.js
+++ src/projects.js
@@ -11,11 +11,17 @@ export const project = (function (title) {
         index !== -1 ? todoItems.splice(index, 1) : console.warn("To do item was not found");
     }
 
+    const getInfoObject = (arr) => {
+        return arr.map(todoItem => {
+            return ({text: todoItem.getText(), date: todoItem.getDate().toString(), priority: todoItem.getPriority().getLevel()});
+        });
+    }
+
     const getToday = () => todoItems.filter(task => task.getDate()?.isDueToday());
 
     const getWeek = () => todoItems.filter(task => task.getDate()?.isDueNextWeek());
 
     const getImportant = () => todoItems.filter(task => task.getPriority()?.getLevel() === "high");
     
-    return { getTitle, setTitle, getTodos, addTodo, removeTodo, getToday, getWeek, getImportant }
+    return { getTitle, setTitle, getTodos, addTodo, removeTodo, getToday, getWeek, getImportant, getInfoObject }
 })();
