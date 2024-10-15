4478a6524deefe71df2382dec61b5f9d57dcadf0
Fabius Gasber <fabiusgasber@gmail.com>
2024-10-15 14:55:20 +0200

    refactor: remove getInfo and toString methods

--- src/projects.js
+++ src/projects.js
@@ -11,21 +11,11 @@ export const project = (function (title) {
         index !== -1 ? todoItems.splice(index, 1) : console.warn("To do item was not found");
     }
 
-    const getInfo = (arr) => {
-        return arr.map(todoItem => {
-            return ({"text": todoItem.getText(), "date": todoItem.getDate().toString(), "priority": todoItem.getPriority().getLevel()});
-        });
-    }
-
-    const toString = (arr) => {
-        return getInfo(arr).map(object => Object.values(object));
-    }
-
     const getToday = () => todoItems.filter(task => task.getDate()?.isDueToday());
 
     const getWeek = () => todoItems.filter(task => task.getDate()?.isDueNextWeek());
 
     const getImportant = () => todoItems.filter(task => task.getPriority()?.getLevel() === "high");
     
-    return { getTitle, setTitle, getTodos, addTodo, removeTodo, getToday, getWeek, getImportant, toString }
+    return { getTitle, setTitle, getTodos, addTodo, removeTodo, getToday, getWeek, getImportant }
 })();
