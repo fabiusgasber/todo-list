0e957cc533c7df3409194c24fd5be33d73dcf6af
Fabius Gasber <fabiusgasber@gmail.com>
2024-10-17 09:56:07 +0200

    refactor: dynamically create to-do form on each add to-do button click

--- src/index.js
+++ src/index.js
@@ -7,22 +7,22 @@ function init() {
 }
 
 const main = domLoader.getQuery("#content");
-const formObj = domCreator.createFormHTMLObj().todoForm;
-const form = domCreator.createTodoContainer("form", formObj);
 const projectFormObj = domCreator.createFormHTMLObj().projectForm;
 const projectForm = domCreator.createTodoContainer("form", projectFormObj);
-form.id = "todo-form";
 
 const setUpListeners = () => {
 
     const button = document.querySelector("#addTodo");
-    button.addEventListener("click", () => domLoader.appendChildToParent(form, main));
-
+    button.addEventListener("click", () => {
+        const formObj = domCreator.createFormHTMLObj().todoForm;
+        const form = domCreator.createTodoContainer("form", formObj);
+        form.id = "todo-form";
+        form.addEventListener("click", (e) => handleClick(e, logicHandler.getLogicObject()?.buttonAction, domLoader.submitForm, domLoader.removeElement));
+        domLoader.appendChildToParent(form, main);
+    });
     const addProjectBtn = document.querySelector("#addProject");
     addProjectBtn.addEventListener("click", () => domLoader.appendChildToParent(projectForm, main));
     projectForm.addEventListener("click", (e) => handleClick(e, logicHandler.getLogicObject()?.buttonAction, domLoader.submitProject, domLoader.removeElement));
-
-    form.addEventListener("click", (e) => handleClick(e, logicHandler.getLogicObject()?.buttonAction, domLoader.submitForm, domLoader.removeElement));
 
     const defaultProjects = Array.from(document.querySelectorAll("li"));
     defaultProjects.forEach(project => project.addEventListener("click", (e) => handleClick(e, logicHandler.getLogicObject()?.navigation)));  
